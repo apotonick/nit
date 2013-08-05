aeb597ff2b4ff66f8e9d26d9069847e87542d098
Nick Sutterer <apotonick@gmail.com>
2013-08-05 23:08:58 +1000

    adding lines.rb :-)

--- lib/nit/lines.rb
+++ lib/nit/lines.rb
@@ -13,6 +13,7 @@ module Nit
         file_patterns.find do |typ, pat|
           if matches = ln.match(pat)
             yield ln, pat, matches
+            true
           end
         end
       end
@@ -22,13 +23,10 @@ module Nit
       files = []
 
       find do |ln, pat, matches|
-        puts "found: #{matches[1]}"
         files << name = matches[1].strip
         # this is for rendering new screen only, don't like it.
-        #yield ln, files.index(name) if block_given? # DISCUSS: pass File object which knows index, line, etc?
+        yield ln, files.index(name) if block_given? # DISCUSS: pass File object which knows index, line, etc?
       end
-
-      puts "files: #{files.inspect}"
 
       files
     end
