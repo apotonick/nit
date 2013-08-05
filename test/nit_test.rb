852dbe56d7a4b49ca5597177a4fb6f29f64e83dc
Nick Sutterer <apotonick@gmail.com>
2013-08-05 14:06:00 +1000

    adding test for status.

--- test/nit_test.rb
+++ test/nit_test.rb
@@ -4,11 +4,16 @@ class NitTest < MiniTest::Spec
   describe "status" do
     it "numbers files to stage" do
       output = nit("status")
-      puts output
+      #puts output
+      output.must_match "modified: [0]    on_stage.rb"
+      output.must_match "modified: [1]    staged.rb"
+      output.must_match "[2] brandnew.rb"
+      output.must_match "[3] new.rb"
+      #puts output
     end
   end
 
   def nit(args)
-    system "bin/nit #{args}"
+    `cd test/dummies/status_1 && ../../../bin/nit #{args}`
   end
 end
